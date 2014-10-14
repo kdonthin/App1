cc7f310fe574e0c66f87546c70f839a565669cac
Kishore Donthineni <kdonthineni@gmail.com>
2014-10-14 16:35:43 -0400

    Show Customer Name and Update Name using RelayCommand in Windows Store App

---
+++ App1/App1.Shared/Models/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1.Models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                if (value != _firstName )
+                {
+                    _firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string LastName
+        { 
+            get 
+            { 
+                return _lastName; 
+            } 
+            set 
+            { 
+                if (value != _lastName)
+                { 
+                    _lastName = value;
+                    OnPropertyChanged();
+                } 
+            } 
+        }
+
+        internal void OnPropertyChanged([CallerMemberName] string member="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(member));
+            }
+        }
+    }
+}
---
+++ App1/App1.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1.Models;
+using App1.Common;
+
+namespace App1.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private RelayCommand _updateNameCommand;
+
+        public Customer MyCustomer { get; set; }
+
+        public MainPageViewModel()
+        {
+            MyCustomer = new Customer() { FirstName = "Kishore", LastName = "Donthineni" };
+        }
+
+        public void UpdateName()
+        {
+            MyCustomer.FirstName = "Radhika" ;
+        }
+
+        public RelayCommand UpdateNameCommand
+        {
+            get
+            {
+                if ( _updateNameCommand == null)
+                {
+					_updateNameCommand = new RelayCommand(UpdateName);
+                }
+
+                return _updateNameCommand;
+            }
+
+            set
+            {
+                _updateNameCommand = value;
+            }
+        }
+    }
+}
